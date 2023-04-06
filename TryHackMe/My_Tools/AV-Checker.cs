3a35a83de7cc456ccc98712756b9f78a3b536ad6
Sugobet <sugob@qq.com>
2023-04-06 11:37:36 +0800

    up

---
+++ TryHackMe/My_Tools/AV-Checker.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Management;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var status = false;
+        Console.WriteLine("[+] Antivirus check is running .. ");
+        string[] AV_Check = { 
+            "MsMpEng.exe", "AdAwareService.exe", "afwServ.exe", "avguard.exe", "AVGSvc.exe", 
+            "bdagent.exe", "BullGuardCore.exe", "ekrn.exe", "fshoster32.exe", "GDScan.exe", 
+            "avp.exe", "K7CrvSvc.exe", "McAPExe.exe", "NortonSecurity.exe", "PavFnSvr.exe", 
+            "SavService.exe", "EnterpriseService.exe", "WRSA.exe", "ZAPrivacyService.exe", "360tray.exe", 
+            "360safe.exe", "ZhuDongFangYu.exe", "360sd.exe", "ksafe.exe", "QQPCRTP.exe", "safedog.exe", 
+            "hipstray.exe", "wsctrl.exe", "usysdiag.exe"
+        };
+        var searcher = new ManagementObjectSearcher("select * from win32_process");
+        var processList = searcher.Get();
+        int i = 0;
+        foreach (var process in processList)
+        {
+            int _index = Array.IndexOf(AV_Check, process["Name"].ToString());
+            if (_index > -1)
+            {
+                Console.WriteLine("--AV Found: {0}", process["Name"].ToString());
+                status = true;
+            }
+            i++;
+        }
+        if (!status) { Console.WriteLine("--AV software is not found!");  }
+    }
+}
